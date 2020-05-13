1226a690a91cf47fb3f87c306dfc8906602ce6a5
anlzou <599502931@qq.com>
2020-05-13 16:33:09 +0800

    init:第一次提交 可以正常编译 存在栈溢出，待修复bug

---
+++ WinApi_Server/WinApi_Server.cpp
@@ -0,0 +1,122 @@
+﻿// WinApi_Server.cpp : 定义应用程序的入口点。
+//
+
+#include "framework.h"
+#include "WinApi_Server.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+///////////////////////////////////////////////////////////////////////////////
+SOCKET sockSer;
+SOCKADDR_IN addrSer,addrCli;
+SOCKET sockConn;
+///////////////////////////////////////////////////////////////////////////////
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    int nErrCode;
+    WORD wVersionReauested;
+    WSADATA wsaData;
+    wVersionReauested = MAKEWORD(2, 2);
+    nErrCode = WSAStartup(wVersionReauested, &wsaData);
+    if (nErrCode != 0)
+    {
+        return -1;
+    }
+    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
+    {
+        WSACleanup();
+        return -1;
+    }
+
+    sockSer = socket(AF_INET, SOCK_STREAM, 0);
+    /////////////////////////////////////////////////////////////////////////
+    DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), NULL, About);
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    char port[10];
+    char ip[20];
+    //char *port = new char[10];
+    //char *ip = new char[20];
+    int len = sizeof(SOCKADDR);
+
+    //char *sendbuf = new char[256];
+    //char *recvbuf = new char[256];
+    char sendbuf[256];
+    char recvbuf[256];
+    ////////////////////////////////////////////////////////////////////////
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+        case WM_INITDIALOG:
+            return (INT_PTR)TRUE;
+
+        case WM_COMMAND:
+        {
+            switch (LOWORD(wParam))// 拦截系统消息
+            {
+            case IDC_QUIT:
+                EndDialog(hDlg, LOWORD(wParam));
+                closesocket(sockSer);
+                WSACleanup();
+                return (INT_PTR)TRUE;
+            case IDC_CREATE:
+                GetDlgItemText(hDlg, IDC_IP, LPWSTR(ip), 20);
+                GetDlgItemText(hDlg, IDC_PORT, LPWSTR(port), 10);
+
+                addrSer.sin_family = AF_INET;
+                addrSer.sin_port = htons(atoi(port));
+                addrSer.sin_addr.S_un.S_addr = inet_addr(ip);
+                bind(sockSer, (SOCKADDR*)&addrSer, len);
+
+                listen(sockSer, 5);
+                sockConn = accept(sockSer, (SOCKADDR*)&addrCli, &len);
+                if (sockConn == INVALID_SOCKET)
+                {
+                    MessageBox(NULL, L"创建服务器 失败", L"创建服务器", 0);
+                }
+                else
+                {
+                    MessageBox(NULL, L"创建服务器 成功", L"创建服务器", 0);
+                }
+                break;
+            case IDC_SEND:
+                GetDlgItemText(hDlg, IDC_SENDBUF, LPWSTR(sendbuf), 256);
+                send(sockConn, sendbuf, strlen(sendbuf) + 1, 0);
+                SetDlgItemText(hDlg, IDC_SENDBUF, LPWSTR(""));
+                break;
+            case IDC_RECV:
+                recv(sockConn, recvbuf, 256, 0);
+                SetDlgItemText(hDlg, IDC_RECVBUF, LPWSTR(recvbuf));
+                break;
+            }
+            if (LOWORD(wParam) == IDOK || LOWORD(wParam)==IDCANCEL)
+            {
+                EndDialog(hDlg, LOWORD(wParam));
+                closesocket(sockSer);
+                WSACleanup();
+                return TRUE;
+            }
+            break;
+        }
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ WinApi_Server/resource.h
@@ -0,0 +1,35 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 WinApi_Server.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDC_QUIT                        3
+#define IDD_ABOUTBOX                    9
+#define IDD_WINAPISERVER_DIALOG         102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINAPISERVER                107
+#define IDI_SMALL                       108
+#define IDC_WINAPISERVER                109
+#define IDR_MAINFRAME                   128
+#define IDC_IP                          1000
+#define IDC_CREATE                      1001
+#define IDC_SEND                        1005
+#define IDC_PORT                        1002
+#define IDC_RECV                        1003
+#define IDC_RECVBUF                     1003
+#define IDC_SENDBUF                     1004
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
